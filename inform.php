6577e25d6d5075b6b2ee9397662a89058daf8e35
aninhabacaninha <2021321666@ifam.edu.br>
2023-10-11 11:25:39 -0400

    telinha de info

--- inform.php
+++ inform.php
@@ -45,8 +45,109 @@
   </nav>
 
   <section class="conteiner">
-    <h2> CONTATOS SEGUROS </h2>
+    <h2> CONTATOS SEGUROS - DELEGACIAS </h2>
 
+    <h3> MANAUS </h3>
+    <h3> - Delegacia da Mulher, localizada na Bola do Eldorado, zona
+      Centro-Sul de Manaus. Aberta 24 horas. </h3>
+    <h3> - Delegacia da Mulher da Zona Sul, localizada na Rua
+      Desembargador Felismino Soares, 155, bairro Colônia
+      Oliveira Machado - funciona de 8h às 17h, de segunda a
+      sexta </h3>
+    <h3> - Delegacia da Mulher da Zona Leste, localizada na Avenida
+      Nossa Senhora da Conceição, bairro Cidade de Deus -
+      funciona de 8h às 17h, de segunda a sexta. </h3>
+    <h3> - Delegacia Especializada em Crimes Contra a Mulher
+      (DECCM), Avenida Mário Ipyranga, 3395, bairro Parque 10,
+      69050-040. </h3>
+    <h3> - Delegacia Especializada em Crimes contra Mulher Anexo,
+      Rua Santa Ana, bairro Cidade de Deus, 69099-262. </h3>
+
+    <h3> Manacapuru </h3>
+    <h3> - Delegacia Interativa de Polícia (DIP), 224, R. Tamandaré, 64 -
+      Aparecida, Manacapuru - AM, 69400-000. Aberto 24 horas. </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso, Rua União, 3000,
+      bairro Aparecida, 69400-456. </h3>
+
+    <h3> São Gabriel da Cachoeira </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso (DECCM), Rua
+      Quatro, 103, bairro Dabaru, 69750-000. </h3>
+
+    <h3> Itacoatiara </h3>
+    <h3> - Delegacia Especializada de Crimes Contra a Mulher
+      (DECCM), Avenida Eduardo Ribeiro, 3581, bairro Jauary 2,
+      69000-100. </h3>
+
+    <h3> Lábrea </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso (DECCM), Rua 24
+      de Agosto, bairro Nossa Senhora de Fátima, 69830-000. </h3>
+
+    <h3> Parintins </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso (DECCM), Rua
+      Irmã Cristine, bairro Itaúna II, 69870-000. </h3>
+
+    <h3> Tabatinga </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso (DECCM), Rua
+      Manoel Tananta, 70, bairro Santa Rosa, 69640-000. </h3>
+
+    <h3> Eirunepé </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada
+      em Crimes Contra a Mulher, Menor e Idoso (DECCM),
+      Estrada Xidá, bairro Nossa Senhora de Fátima, 69880-000. </h3>
+
+    <h3> Tefé </h3>
+    <h3> - Delegacia Especializada em Crimes Contra a Mulher, Rua
+      Estrada do Aeroporto, 1540, bairro Jerusalém, 69470-000. </h3>
+
+    <h3> Coari </h3>
+    <h3> - Delegacia Especializada de Atendimento à Mulher (DEAM),
+      Estrada Coari, bairro Itamarati, 69460-000. </h3>
+
+    <h3> Humaitá </h3>
+    <h3> - Seção de Atendimento à Mulher - Delegacia Especializada da
+      Mulher, do Menor e do Idoso (DEMMI), Rua Vila Lobos, 1100,
+      bairro São José, 69800-000. </h3>
+
+    <h2> CONTATOS SEGUROS - HOPITAIS/UBS </h2>
+
+    <h3> MANAUS </h3>
+    <h3> - UBS S-14, R. Labor, próximo a FItejuta - Educandos, Manaus
+      - AM, 69070-410. </h3>
+    <h3> - UBS Dr. Luiz Montenegro, R. Pico das Águas, 527, bairro -
+      Nossa Sra. das Graças, Manaus - AM, 69053-093. </h3>
+    <h3> - UBSF S-07, R. Xavier de Mendonça, 228 - Nossa Sra.
+      Aparecida, Manaus - AM, 69010-070. </h3>
+    <h3> - Instituto da Mulher Dona Lindú, Avenida Ypiranga, 1580 -
+      Adrianópolis, Manaus - AM, 69057-002. </h3>
+    <h3> - UBSF S-12, R. Santa Isabel - Centro, Manaus - Am,
+      69020-150. </h3>
+    <h3> - Unidade Básica de Saúde S-11 Centro Manaus Amazonas,
+      Avenida Lourenço da Silva Braga - Centro, Manaus - AM,
+      69020-140. </h3>
+    <h3> - UBS S-14, Alameda Rio Mar - Crespo, Manaus - AM.
+      69073-480. </h3>
+    <h3> - UBS Nossa Senhora Auxiliadora (R-39), Comunidade N.S
+      Auxiliadora - Tarumã Mirim, Manaus - Am, 69025-030. </h3>
+    <h3> - UBS S-27, R. Santa Maria - Raiz, Manaus - AM, 69068-270. </h3>
+    <h3> - UBS O-22, R. Manaus - Compensa 2, Manaus - AM,
+      69036-160. </h3>
+    <h3> - UBS S-53, Rua 3, Conjunto Beija Flôr - Flores, Manaus - AM,
+      69028-402. </h3>
+    <h3> - UBS S-32, Rua Gen. Glicério - Cachoeirinha, Manaus - AM,
+      69065-140. </h3>
+    <h3> - UBS S-32, Rua Gen. Glicério - Cachoeirinha, Manaus - AM,
+      69065-140. </h3>
+    <h3> - UBS Ida Mentori, Rua Ajuricaba - São Jorge, Manaus - AM,
+      69083-020. </h3>
+    <h3> - UBS S-35, Tv. Nova, 5 - Petrópolis, Manaus - AM, 69067-001. </h3>
+    <h3> - USF Gebes Medeiros (UBS), Avenida Pirarucu, 100 - Jorge
+      Teixeira, Manaus - AM, 69088-765. </h3>
+    <br> <br>
   </section>
 
   <!--Tela de login e cadastro-->
