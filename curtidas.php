db0138d0e39c6d7267513ba83bb43d256808315c
aninhabacaninha <2021321666@ifam.edu.br>
2023-10-11 15:04:52 -0400

    sos sos sos vapo vapo qual foi

---
+++ curtidas.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+  <title> Curtidas </title>
+  <link href="img/logos.png" rel="icon">
+  <link href="css/estilo.css" rel="stylesheet">
+
+  <!--fontes-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter&display=swap" rel="stylesheet">
+
+  <!--JavaScript-->
+  <script type="text/javascript" src="../js/Cod.js"></script>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <header class="perfiheader">
+    <!-- pra botao pra editar la no coisa -->
+    <img src="img/perfil.png" width="150px" height="150px">
+  </header>
+  <nav class="menu">
+    <ul>
+      <li><a href="perfil.php"> Postagens </a></li>
+      <li class="selecionado"><a href="curtidas.php"> Curtidas </a></li>
+    </ul>
+  </nav>
+
+  <footer>
+    <!-- no ta fumcionadi sos sos sos atencao galera-->
+    <button id="btPublicar" class="btPubli" type="button" href="postagens.php"> + </button>
+  </footer>
+</body>
+
+</html>
---
+++ postagens.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title> Postagens </title>
+  <link href="img/logos.png" rel="icon">
+  <link href="css/estilo.css" rel="stylesheet">
+
+  <!--fontes-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter&display=swap" rel="stylesheet">
+
+  <!--JavaScript-->
+  <script type="text/javascript" src="../js/Cod.js"></script>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <!--abri essa porra pra editar so que a milly ainda nao fez la no figma enrao nao sei como faz sos sos sos-->
+  <h3> teste </h3>
+</body>
+
+</html>
