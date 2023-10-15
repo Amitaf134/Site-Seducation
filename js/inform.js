97900a9439f01a16ee066e249a4ae4cb79eefd87
Amitaf134 <2021320856@ifam.edu.br>
2023-10-15 14:49:36 -0400

    agora sim

--- js/inform.js
+++ js/inform.js
@@ -4,9 +4,9 @@ function mostrarInformsH(){
     var informs = document.getElementById('informHos').style.display;
      soma = soma + 1;
      if(soma % 2 == 0){
-        document.getElementById('informHos').style.display = 'none';
-     }else{
         document.getElementById('informHos').style.display = 'block';
+     }else{
+        document.getElementById('informHos').style.display = 'none';
      }
 }
 
@@ -14,8 +14,8 @@ function mostrarInformsD(){
     var informs = document.getElementById('informDel').style.display;
      soma = soma + 1;
      if(soma % 2 == 0){
-        document.getElementById('informDel').style.display = 'none';
-     }else{
         document.getElementById('informDel').style.display = 'block';
+     }else{
+        document.getElementById('informDel').style.display = 'none';
      }
 }
